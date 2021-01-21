c0b7836668607c7b232300842a7f82d4978e6fc7
Vilmantas Petrusevičius <aaa>
2021-01-21 16:20:13 +0100

    State pattern init.

---
+++ Pattern.State/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pattern.State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
